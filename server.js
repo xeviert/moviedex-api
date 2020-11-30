cdaa2dcd9a6965efb584fd8b67e2d1ef9dfbdc86
Xevier Turrubiartes <xevierturrubiartes@gmail.com>
2020-11-30 17:21:31 -0600

    bearer token setup

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ const app = express()
 app.use(morgan('dev'))
 
 app.use(function validateBearerToken(req, res, next) {
-  console.log('validate bearer token middleware')
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+
   next()
 })
 
