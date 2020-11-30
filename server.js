54933a4849555504eca244aa0310025094075feb
Xevier Turrubiartes <xevierturrubiartes@gmail.com>
2020-11-30 16:58:46 -0600

    halfway checkpoint

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const MOVIES = require('./movies-data-small.json')
+
+console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
 
-app.use((req, res) => {
-  res.send('Hello, world!')
+app.use(function validateBearerToken(req, res, next) {
+  console.log('validate bearer token middleware')
+  next()
 })
+
+function handleGetMovie(req, res) {
+  res.json(MOVIES)
+}
+
+app.get('/movie', handleGetMovie)
+
 
 const PORT = 8000
 
