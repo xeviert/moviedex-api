bd15582ea698cd6c576056c3c056f62590d7bcb5
Xevier Turrubiartes <xevierturrubiartes@gmail.com>
2020-11-30 18:25:47 -0600

    need helmet

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
 const MOVIES = require('./movies-data-small.json')
-
-console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(helmet());
+app.use(cors())
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
@@ -20,11 +22,28 @@ app.use(function validateBearerToken(req, res, next) {
   next()
 })
 
-function handleGetMovie(req, res) {
-  res.json(MOVIES)
-}
+app.get('/movie', function handleGetMovie(req, res) {
+  let response = MOVIES;
 
-app.get('/movie', handleGetMovie)
+  if (req.query.genre) {
+    response = response.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    )
+  }
+  if (req.query.country) {
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    )
+  }
+  if (req.query.avg_vote) {
+    response = response.filter(movie =>
+      Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    )
+  }
+
+
+  res.json(response)
+})
 
 
 const PORT = 8000
