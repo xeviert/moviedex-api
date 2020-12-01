e7f5fd90edf2bca349319dda6eb9caf92e9fdedd
Xevier Turrubiartes <xevierturrubiartes@gmail.com>
2020-11-30 18:49:16 -0600

    final commit

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const helmet = require('helmet')
-const cors = require('cors')
-const MOVIES = require('./movies-data-small.json')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const MOVIES = require('./movies-data-small.json');
 
 const app = express()
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 app.use(helmet());
-app.use(cors())
+app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
@@ -28,12 +28,12 @@ app.get('/movie', function handleGetMovie(req, res) {
   if (req.query.genre) {
     response = response.filter(movie =>
       movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
-    )
+    );
   }
   if (req.query.country) {
     response = response.filter(movie =>
       movie.country.toLowerCase().includes(req.query.country.toLowerCase())
-    )
+    );;
   }
   if (req.query.avg_vote) {
     response = response.filter(movie =>
@@ -41,10 +41,8 @@ app.get('/movie', function handleGetMovie(req, res) {
     )
   }
 
-
   res.json(response)
 })
-
 
 const PORT = 8000
 
