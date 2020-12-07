20a2275d53434eebcb89196ab263bd183790b0e8
Xevier Turrubiartes <xevierturrubiartes@gmail.com>
2020-12-07 14:09:23 -0600

    prepare for Heroku

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const MOVIES = require('./movies-data-small.json');
 
 const app = express()
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting))
 app.use(helmet());
 app.use(cors());
 
@@ -33,7 +34,7 @@ app.get('/movie', function handleGetMovie(req, res) {
   if (req.query.country) {
     response = response.filter(movie =>
       movie.country.toLowerCase().includes(req.query.country.toLowerCase())
-    );;
+    );
   }
   if (req.query.avg_vote) {
     response = response.filter(movie =>
@@ -44,7 +45,17 @@ app.get('/movie', function handleGetMovie(req, res) {
   res.json(response)
 })
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
